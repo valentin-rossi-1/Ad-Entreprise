285fe21ed123ec64925dac4cef224bf552e1eaaa
Val_laplat <valentin.rossi@laplateforme.io>
2025-11-04 11:24:23 +0100

    Update Azure connection script to use device authentication

--- scripts/Connect-Azure.ps1
+++ scripts/Connect-Azure.ps1
@@ -1,11 +1,22 @@
+# Ouvrez le fichier Connect-Azure.ps1 dans un éditeur de texte
+# Remplacez le contenu par ce qui suit
+
 # Script de connexion à Azure et Microsoft Graph (Entra ID)
 # ---------------------------------------------------
 
-# Connexion à Azure
-Connect-AzAccount
+# Connexion à Azure avec authentification par code d'appareil
+Write-Host "Connexion à Azure..." -ForegroundColor Green
+Connect-AzAccount -UseDeviceAuthentication
 
-# Connexion à Microsoft Graph avec les autorisations nécessaires
-Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All", "Application.ReadWrite.All", "Policy.ReadWrite.All"
+# Connexion à Microsoft Graph avec authentification interactive
+Write-Host "Connexion à Microsoft Graph..." -ForegroundColor Green
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Group.ReadWrite.All", "Application.ReadWrite.All", "Policy.ReadWrite.All" -UseDeviceAuthentication
 
 # Afficher les informations du tenant connecté
-Get-MgOrganization | Select-Object DisplayName, Id
+Write-Host "Récupération des informations du tenant..." -ForegroundColor Green
+try {
+    Get-MgOrganization | Select-Object DisplayName, Id
+    Write-Host "Connexion réussie!" -ForegroundColor Green
+} catch {
+    Write-Host "Erreur lors de la récupération des informations du tenant: $_" -ForegroundColor Red
+}
